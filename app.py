2e646a492730c31a54ca487478f8f55cc67ea683
lee nam kyeong <skarod0401@naver.com>
2022-08-16 00:21:24 +0900

    문장 전달post

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 
@@ -7,6 +7,25 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 def index():
     return render_template('index.html')
 
+@app.route('/photo_to_song',methods=['POST'])
+def post():
+	value = dict(request.form)
+	print(value)
+	return value
+#         root_path()
+#         # Reference Image
+#         refer_img = request.form['refer_img']
+#         refer_img_path = '/images/nst_get/' + str(refer_img)
+#
+#         # User Image (target image)
+#         user_img = request.files['user_img']
+#         user_img.save('./flask_deep/static/images/' + str(user_img.filename))
+#         user_img_path = '/images/' + str(user_img.filename)
+#
+#         # Neural Style Transfer
+#         transfer_img = neural_style_transfer.main(refer_img_path, user_img_path)
+#         transfer_img_path = '/images/' + str(transfer_img.split('/')[-1])
+#     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
